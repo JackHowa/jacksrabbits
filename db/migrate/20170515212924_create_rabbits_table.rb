057aadd76b793347d8ef58dd6a6f0d0d42079b06
Jack Howard <johncarverhoward@gmail.com>
2017-05-16 08:22:27 -0500

    refactor associations

--- db/migrate/20170515212924_create_rabbits_table.rb
+++ db/migrate/20170515212924_create_rabbits_table.rb
@@ -1,10 +1,10 @@
 class CreateRabbitsTable < ActiveRecord::Migration[5.0]
   def change
   	create_table :rabbits do |t|
-  		t.string :name, null: false 
-  		t.string :color 
+  		t.string :name, null: false
+  		t.string :color
 
-  		t.timestamps null: false 
-  	end 
+  		t.timestamps null: false
+  	end
   end
 end
--- db/migrate/20170516072852_create_walks_table.rb
+++ db/migrate/20170516072852_create_walks_table.rb
@@ -1,13 +1,11 @@
 class CreateWalksTable < ActiveRecord::Migration[5.0]
   def change
 	  create_table :walks do |t|
-      t.integer :user_id,  :null => false
-      t.integer :rabbit_id, :null => false
+      t.references :user
+      t.references :rabbit
+      t.string :location
 
       t.timestamps
     end
-
-    add_index :walks, [:user_id, :rabbit_id], :unique => true
-    add_index :walks, :rabbit_id
   end
 end

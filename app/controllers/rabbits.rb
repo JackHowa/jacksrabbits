9dbf19f73b97bf452e08be94e30c692bb995ee39
jackhowa <johncarverhoward@gmail.com>
2017-05-15 21:28:04 -0500

    start show all rabbits slice

--- app/controllers/rabbits.rb
+++ app/controllers/rabbits.rb
@@ -7,3 +7,9 @@ get '/rabbits' do
 	@rabbits = Rabbit.all
 	erb :"rabbits/index"
 end 
+
+# show one rabbit
+get '/rabbits/:rabbit_id' do 
+	@rabbit = Rabbit.find_by(id: params[:id])
+	erb :"channels/show"
+end 
